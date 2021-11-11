d55340cc370dd0ee21a8ad32c90b9a12d5d2f85f
lucassilva <lucassds96@outlook.com>
2021-11-11 18:30:54 -0300

    adding providers

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -3,7 +3,12 @@ package daniel.lop.io.marvelappstarter.util
 object Constants {
 
     const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+
     const val PUBLIC_KEY = "b636f090efad253b7837ffd2d45a08ce"
     const val PRIVATE_KEY = "440c47ce82ba7d10ba0a58a8228a3fb650ebd437"
+
+    const val API_KEY = "440c47ce82ba7d10ba0a58a8228a3fb650ebd437"
+    const val HASH = "440c47ce82ba7d10ba0a58a8228a3fb650ebd437"
+    const val TS = "440c47ce82ba7d10ba0a58a8228a3fb650ebd437"
 
 }
