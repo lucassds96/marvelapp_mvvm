c40060736c7ccbf1c7c6bf2225d73424a1120a69
lucassilva <lucassds96@outlook.com>
2021-11-11 17:11:52 -0300

    adding service api

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,9 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+    const val PUBLIC_KEY = "b636f090efad253b7837ffd2d45a08ce"
+    const val PRIVATE_KEY = "440c47ce82ba7d10ba0a58a8228a3fb650ebd437"
+
+}
