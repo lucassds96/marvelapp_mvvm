60e2ec4ad7e622cc77ab19fb7e3db6f3da78c712
lucassilva <lucassds96@outlook.com>
2021-11-11 15:25:49 -0300

    adding navigation component

--- app/src/main/java/daniel/lop/io/marvelappstarter/MainActivity.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/MainActivity.kt
@@ -2,15 +2,30 @@ package daniel.lop.io.marvelappstarter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import daniel.lop.io.marvelappstarter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var naviHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        naviHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = naviHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {  }
+        }
     }
 }
